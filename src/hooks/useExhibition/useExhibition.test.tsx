060aee2c883a8707ff81d9b49f3e5f40e6949196
Aly Filekesh <alifilekesh@gmail.com>
2022-02-04 01:53:30 +0330

    feat: add integration test of useExhibition

---
+++ src/hooks/useExhibition/useExhibition.test.tsx
@@ -0,0 +1,33 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useExhibition from './useExhibition';
+import { createWrapper } from 'test-utils/wrapper';
+import { server } from 'mocks/server';
+import { exhibitionHandlerException } from 'mocks/handlers';
+
+import getMockExhibition from 'test-utils/mockedData/exhibition/getMockExhibition';
+
+let exhibitionId = 9252;
+describe('testing useExhibition hook', () => {
+  test('successful query hook', async () => {
+    const { result, waitFor } = renderHook(() => useExhibition(exhibitionId), {
+      wrapper: createWrapper(),
+    });
+    await waitFor(() => {
+      return result.current.isSuccess;
+    });
+
+    expect(result.current.data).toEqual({ ...getMockExhibition(exhibitionId) });
+  });
+
+  test('failure query hook', async () => {
+    exhibitionId = 12;
+    server.use(exhibitionHandlerException);
+    const { result, waitFor } = renderHook(() => useExhibition(exhibitionId), {
+      wrapper: createWrapper(),
+    });
+
+    await waitFor(() => result.current.isError);
+
+    expect(result.current.error).toBeDefined();
+  });
+});
