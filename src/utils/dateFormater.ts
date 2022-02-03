b109dc14a3385b78f461cf6b165f89361b73bfd3
Aly Filekesh <alifilekesh@gmail.com>
2022-02-04 01:47:57 +0330

    fix: some issue

--- src/utils/dateFormater.ts
+++ src/utils/dateFormater.ts
@@ -1,5 +1,6 @@
 export const EDATEwFormat = (date: string) => {
   let d = new Date(date);
+  
   let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
   let mo = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(d);
   let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
