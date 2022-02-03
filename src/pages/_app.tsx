b4bfbd443b01d0de7ebd22756d814d919baebb61
Aly Filekesh <alifilekesh@gmail.com>
2022-02-04 01:35:50 +0330

    update custom app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css';
+import { useState, ReactElement, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import '../styles/globals.css';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = useState(() => new QueryClient());
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        {getLayout(<Component {...pageProps} />)}
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
